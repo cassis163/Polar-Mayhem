87935abf602693082cb60f5f27912f032c277750
joverwijk <jarnooverwijk@gmail.com>
2019-08-27 12:38:54 +0200

    Add Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,21 @@
-public class Animal {
+public abstract class Animal {
+    private boolean alive = true;
+    private int energy;
+    private int energyThreshold;
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void feed(int energy) {
+        energy += energy;
+    }
+
+    public void die() {
+        alive = false;
+    }
+
+    public boolean isStarving() {
+        return (alive == true && energy < energyThreshold);
+    }
 }
