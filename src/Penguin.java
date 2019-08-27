3cb3320c361a1f2dee27cd6075e9cf3ded233414
joverwijk <jarnooverwijk@gmail.com>
2019-08-27 12:46:33 +0200

    Add Penguin.java & PolarBear.java

--- src/Penguin.java
+++ src/Penguin.java
@@ -1,2 +1,3 @@
-public class Penguin {
+public class Penguin extends Animal {
+
 }
--- src/PolarBear.java
+++ src/PolarBear.java
@@ -1,2 +1,11 @@
-public class PolarBear {
+public class PolarBear extends Animal {
+    public void hunt(Penguin[] penguins) {
+        Penguin penguin = penguins[MathHelper.getRandomNumber(0, penguins.length)];
+
+        hunt(penguin);
+    }
+
+    public void hunt(Penguin penguin) {
+        penguin.die();
+    }
 }
