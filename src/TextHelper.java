04bb4522107ba2e737c6566864f5d1278b31162a
Daniel de Cloet <djdecloet@gmail.com>
2019-08-27 12:05:17 +0200

    Add base TextHelper methods

---
+++ src/TextHelper.java
@@ -0,0 +1,21 @@
+public class TextHelper {
+    public void println(String message) {
+        System.out.println(message);
+    }
+
+    public void showWelcome() {}
+
+    public void showInstructions() {}
+
+    public void showFullscreen() {}
+
+    public void showNoPenguinToHunt() {}
+
+    public void showNoPenguinFed() {}
+
+    public void showBye() {}
+
+    public void showInvalidCommand(String cmd) {}
+
+    public void showStatus() {}
+}
